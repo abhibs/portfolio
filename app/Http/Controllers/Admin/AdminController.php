1e0b4195cfafeed7825baafa983bf81e594838f8
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-09 02:37:52 +0530

    admin login page created

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('admin.login');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/test', function () {
     echo "Abhiram";
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/login', [AdminController::class, 'login'])->name('admin-login');
+    Route::group(['middleware' => 'auth:admin'], function () {
+
+    });
 });
