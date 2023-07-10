a74548e94e8cc1ed57b6154bd39eceb1fdec25bc
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-10 14:40:54 +0530

    navbar scrip add .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,9 +79,14 @@
 
 
 
-    <script src="{{ asset('user/assets/js/jquery.js') }}"></script>
-    <script src="{{ asset('user/assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('user/assets/fontawesome/js/all.min.js') }}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     <script src="{{ asset('user/assets/js/script.js') }}"></script>
+     <script src="{{ asset('user/assets/js/bootstrap.bundle.min.js') }}"></script>
+     <script src="{{ asset('user/assets/fontawesome/js/all.min.js') }}"></script>
+    <!--  isotope-->
+    <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
 </body>
 
 </html>
