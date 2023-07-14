544417800c58742006887754cfe43290bcb45ec1
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-15 02:23:07 +0530

    resume api controller created

--- resources/views/admin/resume/index.blade.php
+++ resources/views/admin/resume/index.blade.php
@@ -60,7 +60,7 @@
                                     <div class="row">
                                         <div class="col-sm-3"></div>
                                         <div class="col-sm-9 text-secondary">
-                                            <input type="submit" class="btn btn-primary px-4" value="Add Resume" />
+                                            <input type="submit" class="btn btn-primary px-4" value="Upadate Resume" />
                                         </div>
                                     </div>
                             </div>
